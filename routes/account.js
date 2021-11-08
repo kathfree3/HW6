5bb8cb9f8551e1889e17c7fd5ddad7068c5a1573
kathfree3 <kfree3@seas.upenn.edu>
2021-11-08 12:36:16 -0500

    user routes set up very basic

--- routes/account.js
+++ routes/account.js
@@ -1,17 +1,52 @@
 // routes to handle the log in system
-
-// '/signup' POST
-
-// '/login' POST
-
-// '/logout' POST
-
 const express = require('express')
+const User = require('../models/user')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('all the projects in your repo')
+  console.log('hi')
+  res.send('idk')
+})
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const user = await User.findOne({ username })
+    console.log(user)
+    if (!user) {
+      res.send('user doses not exist')
+    } else {
+      const { password: passDB } = user
+      if (password === passDB) {
+        req.session.username = username
+        req.session.password = password
+        res.send('user logged in successfully')
+      } else {
+        res.send('user credentials are wrong')
+      }
+    }
+  } catch (err) {
+    console.log(err)
+    res.send('user creation has problems')
+  }
+})
+
+router.post('/signup', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    await User.create({ username, password })
+    res.send('user created')
+  } catch (err) {
+    console.log(err)
+    res.send('user creation has problems')
+  }
+})
+
+router.post('/logout', async (req, res) => {
+  req.session.username = null
+  req.session.password = null
+  res.send('user is logged out')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const mongoose = require('mongoose')
 const express = require('express')
+const session = require('cookie-session')
+const mongoose = require('mongoose')
+
+const accountRouter = require('./routes/account')
 
 const app = express()
 
-const my_url = 'mongodb+srv://dbuser:dbpassword@cluster0.pm7lg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const my_url = 'mongodb+srv://dbuser:dbpassword@cluster0.pm7lg.mongodb.net/hw-db?retryWrites=true&w=majority'
 const MONGO_URI = process.env.MONGODB_URI || my_url
-
-const accountRouter = require('./routes/account')
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 
-app.listen(express.json())
+app.use(express.json())
+
+app.use(session({
+  name: 'session',
+  keys: ['key1', 'key2'],
+}))
 
 app.use('/account', accountRouter)
 
